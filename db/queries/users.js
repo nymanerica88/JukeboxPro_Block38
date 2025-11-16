cbd7320e2fbc35d427cebbd21ce16107b7abf995
Erica Nyman <ervin.erica29@gmail.com>
2025-11-16 18:38:18 -0500

    feat: added getUserByUsername and getUserById

--- db/queries/users.js
+++ db/queries/users.js
@@ -2,19 +2,55 @@ import bcrypt from "bcrypt";
 import db from "#db/client";
 
 export async function createUser(username, password) {
-    try {
-        const sql =`
+  try {
+    const sql = `
         INSERT INTO users (username, password)
         VALUES ($1, $2)
         RETURNING *;
         `;
 
-        const hashPassword = await bcrypt.hash(password, 10);
-        const {
-            rows: [user],
-        } = await db.query(sql, [username, hashPassword]);
-        return user;
-    } catch (error) {
-        console.error("There was an error creating user", error);
-    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const {
+      rows: [user],
+    } = await db.query(sql, [username, hashPassword]);
+    return user;
+  } catch (error) {
+    console.error("There was an error creating user", error);
+  }
+}
+
+export async function getUserByUsername(username) {
+  try {
+    const sql = `
+        SELECT *
+        FROM users
+        WHERE username = $1;
+        `;
+
+    const values = [username];
+    const {
+      rows: [user],
+    } = await db.query(sql, values);
+    return user || null;
+  } catch (error) {
+    console.error(`Error getting User`, error);
+  }
+}
+
+export async function getUserById(id) {
+  try {
+    const sql = `
+        SELECT *
+        FROM users
+        WHERE id = $1;
+        `;
+
+    const values = [id];
+    const {
+      rows: [user],
+    } = await db.query(sql, values);
+    return user || null;
+  } catch (error) {
+    console.error("Error getting user by ID", error);
+  }
 }
