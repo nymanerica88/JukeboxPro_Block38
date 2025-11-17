66ee8e18527737d14957c6e32c7fba7c48858f03
Erica Nyman <ervin.erica29@gmail.com>
2025-11-16 22:10:05 -0500

    feat: added/created GET /tracks/:id/playlists

--- api/tracks.js
+++ api/tracks.js
@@ -3,6 +3,8 @@ const router = express.Router();
 export default router;
 
 import { getTracks, getTrackById } from "#db/queries/tracks";
+import requireUser from "#middleware/requireUser";
+import { getPlaylistByTrackAndUser } from "#db/queries/playlists_tracks";
 
 router.get("/", async (req, res) => {
   const tracks = await getTracks();
@@ -13,4 +15,21 @@ router.get("/:id", async (req, res) => {
   const track = await getTrackById(req.params.id);
   if (!track) return res.status(404).send("Track not found.");
   res.send(track);
+});
+
+router.get("/:id/playlists", requireUser, async (req, res, next) => {
+  try {
+    const trackId = Number(req.params.id);
+    if (!Number.isInteger(trackId))
+      return res.status(400).send("Invalid track id");
+
+    const track = await getTrackById(trackId);
+    if (!track) return res.status(404).send("Track not found.");
+
+    const playlists = await getPlaylistByTrackAndUser(trackId, req.user.id);
+    res.status(200).send(playlists);
+  } catch (error) {
+    console.error(`Error fetching playlists for track`, error);
+    next(error);
+  }
 });
