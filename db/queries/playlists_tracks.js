b90f49289a881c721231413899a744eeb698fbcb
Erica Nyman <ervin.erica29@gmail.com>
2025-11-16 22:11:14 -0500

    feat: created & debugged getPlaylistByTrackAndUser

--- db/queries/playlists_tracks.js
+++ db/queries/playlists_tracks.js
@@ -13,3 +13,22 @@ export async function createPlaylistTrack(playlistId, trackId) {
   } = await db.query(sql, [playlistId, trackId]);
   return playlistTrack;
 }
+
+export async function getPlaylistByTrackAndUser(trackId, userId) {
+  try {
+    const sql = `
+    SELECT playlists.id, playlists.name, playlists.description, playlists.user_id
+    FROM playlists
+    INNER JOIN playlists_tracks ON playlists.id = playlists_tracks.playlist_id
+    WHERE playlists_tracks.track_id = $1
+      AND playlists.user_id = $2
+  `;
+    const values = [trackId, userId];
+
+    const { rows } = await db.query(sql, values);
+    return rows;
+  } catch (error) {
+    console.error("Error in getPlaylistByTrackAndUser:", error);
+    throw error;
+  }
+}
